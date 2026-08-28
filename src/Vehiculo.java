cdb4a45ed6bfa0cf352638574cd1cb694510ff32
samuelvera <samuvera08@gmail.com>
2026-08-28 07:47:50 -0500

    creo clase vehiculo y arreglo error de clase publica

---
+++ src/Vehiculo.java
@@ -0,0 +1,66 @@
+public class Vehiculo {
+
+    protected String marca;
+    protected int anioFabricacion;
+    protected double precioBase;
+    protected boolean importado;
+
+    public Vehiculo() {
+        marca = "Sin marca";
+        anioFabricacion = 2024;
+        precioBase = 0.0;
+    }
+}
+
+        public Vehiculo(String marca, int anioFabricacion) {
+            this.marca = marca;
+            this.anioFabricacion = anioFabricacion;
+            precioBase = 50000000;
+            importado = false;
+        }
+
+        public Vehiculo(String marca, int anioFabricacion, double precioBase) {
+            this.marca = marca;
+            this.anioFabricacion = anioFabricacion;
+            this.precioBase = precioBase;
+            importado = false;
+        }
+
+        public Vehiculo(String marca, int anioFabricacion, double precioBase, boolean importado) {
+            this.marca = marca;
+            this.anioFabricacion = anioFabricacion;
+            this.importado = importado;
+            this.precioBase = precioBase * (importado = true ? 1.19 : 1.0);
+        }
+
+        public String getMarca() {
+            return marca;
+        }
+
+        public double getPrecioBase() {
+            double precioBase = 0;
+            return precioBase;
+        }
+
+        public boolean esImportado() {
+            return importado;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            Vehiculo otro = (Vehiculo) obj;
+            return this.marca == otro.marca;
+        }
+
+        @Override
+        public String toString() {
+            return "Vehiculo [marca=" + marca + ", anio=" + anioFabricacion
+                    + ", precioBase=" + precioBase + ", importado=" + importado + "]";
+        }
+    }
+
+
+
+
+
+}
